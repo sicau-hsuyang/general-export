3205094faf92046b1af583d49acaf399b2a35518
johnyang <404189928@qq.com>
2020-01-10 23:31:06 +0800

    draft:准备处理excel导出

--- libs/implements/xlsx-component.js
+++ libs/implements/xlsx-component.js
@@ -40,33 +40,64 @@ export default class XlsxComponent extends BaseComponent {
   }
 
   async doWExport() {
-    let excelBlob = null
     let dataSource = this.config.data;
     this.makeSureArray(dataSource)
-    const data = reshapeData(dataSource)
-    const excel = new Excel.Workbook()
-    excel.created = new Date()
+    const data = this.reshapeData(dataSource);
+    const excel = new Excel.Workbook();
+    excel.created = new Date();
     excel.creator = "Create by general-export lib"
-    const sheet = excel.addWorksheet('Table')
-    const exportProps = get()
-    const header = exportProps.length === 0 ? Object.keys(data[0]).map(key => {
-      return { prop: key, label: key }
-    }) : exportProps
-    sheet.columns = header.map(col => {
+    const sheet = excel.addWorksheet("Table");
+    const outColumns = this.getVisibleProps();
+    // 对象的字段顺序映射
+    const columnSequnceMapping = {};
+    const header = outColumns.length === 0
+      ? Object.keys(data[0]).map(prop => {
+        return { prop: prop, label: prop };
+      }) : outColumns;
+    sheet.columns = header.map((col, colIdex) => {
+      columnSequnceMapping[col.prop] = colIdex;
       return {
         key: col.prop,
         header: col.label
-      }
-    })
-    sheet.addRows(data)
-    try {
-      const buffer = await excel.xlsx.writeBuffer()
-      excelBlob = new Blob([buffer])
-    } catch (exp) {
-      console.error('[ the error has occurred when exporting or the browser can not support export]')
-    }
-
-    return excelBlob
+      };
+    });
+    // 因为多了一行 所以处理要从第二个开始
+    sheet.addRows(data);
+    // 此时必须还是要拿dataSource取判断
+    dataSource.forEach((row, rowIdx) => {
+      Object.entries(row).forEach(([prop, propValue]) => {
+        const colNum = this.getColCellNum(
+          columnSequnceMapping[prop] + 1 // 1-> A 因为拿到的是序号所以需要+1
+        );
+        // 因为第一行用的是表头 所以行号直接是从第二行开始算
+        const rowNum = rowIdx + 2;
+        const cellAddr = colNum + "" + rowNum;
+        if (propValue.colSpan > 1 && prop.rowSpan > 1) {
+          // 合并行列
+          // 同时合并行 和 列
+        } else if (propValue.rowSpan > 1) {
+          // 合并行
+          // 合并行的时候因为算了自己 所以要减1
+          const endRowNum =
+            rowNum + propValue.rowSpan - 1;
+          const endAddr = colNum + "" + endRowNum;
+          sheet.mergeCells(cellAddr + ":" + endAddr);
+          let cell = sheet.getCell(cellAddr);
+          cell && (cell.alignment.horizontal = 'middle')
+        } else if (propValue.colSpan > 1) {
+          // 合并列
+          // 合并行的时候因为算了自己 所以要减1
+          const endColNum = colNum + propValue.colSpan - 1
+          const endAddr = rowNum + "" + endColNum
+          sheet.mergeCells(cellAddr + ":" + endAddr);
+          let cell = sheet.getCell(cellAddr);
+          cell && (cell.alignment.vertical = 'center')
+        }
+      });
+    });
+    let buffer = await excel.xlsx.writeBuffer();
+    //console.error('[ the error has occurred when exporting or the browser can not support export]')
+    return new Blob([buffer]);
   }
 
 }
